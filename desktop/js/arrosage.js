68c6a2d29870193d1b988ef2bf61e9cc3247b167
vegeta0911 <67876957+vegeta0911@users.noreply.github.com>
2025-08-11 23:53:07 +0200

    Update arrosage.js

--- desktop/js/arrosage.js
+++ desktop/js/arrosage.js
@@ -192,13 +192,23 @@ function addArrosage(un_arrosage){
 debugA=null;
 debugB=null;
 
-  function ajouter_programmation(un_arrosage,une_prog){
+  function ajouter_programmation(_eqLogic,un_arrosage,une_prog){
+    
+  const infos = _eqLogic.cmd
+    .filter(c => c.subType === 'string' && c.type === 'info');
+
+  const allArrosages = $('.un_arrosage');
+
+  const idx = allArrosages.index(un_arrosage);
+
+  const state = (idx >= 0 && idx < infos.length) ? infos[idx].state : '';
+
+  console.log(state);
 
     if(typeof une_prog === "string"){
       une_prog=une_prog.replaceAll('"','&quot;')
     }
 
-    
     var div = '<div class="form-group une_programmation" >';
     div += '<label class="col-sm-2 control-label">{{Programmation}}</label>';
     div += '<div class="col-sm-7"  >';
@@ -215,9 +225,15 @@ debugB=null;
     div += '</div>';
     div += '</div>';
     div += ' </div>';
-
+    div += '<div class="form-group">';
+    div += '<label class="col-sm-2 control-label">{{Prochaine execution}}</label>';
+    div += '<div class="col-sm-2">';
+    div += '<span class="control-label label-success" >'  +  state  +  '</span>';
+    div += '</div>';
+    div += ' </div>';
     var el=un_arrosage.find('.declencheur').last().parent().parent();
     el.after(div)
+    
   }
 
   function ajouter_timer(un_arrosage,element){
@@ -323,7 +339,7 @@ debugB=null;
   $("body").off('click','.b_add_programmation').on('click','.b_add_programmation',function () {
     modifyWithoutSave=true;
     var el = $(this).parent().parent();
-    ajouter_programmation(el,'')
+    ajouter_programmation(_eqLogic,el,'')
   });
   $("body").off('click','.remove_programmation').on('click','.remove_programmation',function () {
     modifyWithoutSave=true;
@@ -425,7 +441,7 @@ $('body').off('focusout','.cmdAction.expressionAttr[data-l1key=cmd]').on('focuso
   var type = $(this).attr('data-type');
   var expression = $(this).parent().getValues('.expressionAttr');
   var el = $(this).parent().parent().parent();
-  console.log(expression)
+  console.log(type.expression)
   debugC=$(this)
   jeedom.cmd.displayActionOption($(this).value(), init(expression[0].options), function (html) {
     el.find('.actionOptions').html(html);
@@ -444,8 +460,6 @@ function load_arrosage(_eqLogic){
         for (var i in _eqLogic.configuration.liste_arrosage) {
             var un_arrosage=_eqLogic.configuration.liste_arrosage[i]
             var arrosage=addArrosage(_eqLogic.configuration.liste_arrosage[i]);
-            // console.log('arrosage')
-            // console.log(un_arrosage)
             var mode=arrosage.find('.declencheur').last()
             debug=un_arrosage
             mode.val(un_arrosage.declencheur);
@@ -454,7 +468,7 @@ function load_arrosage(_eqLogic){
                 ajouter_declencheur(arrosage,element,null,'Déclencheur')
             });
             (un_arrosage.liste_programmation).forEach(element => {
-                ajouter_programmation(arrosage,element)
+                ajouter_programmation(_eqLogic,arrosage,element)
             });
             (un_arrosage.liste_an_declencheur).forEach(element => {
                 ajouter_declencheur(arrosage,element,'an_declencheur','')
@@ -492,6 +506,7 @@ function save_arrosage(_eqLogic){
 
     _eqLogic.configuration.liste_arrosage = [];
     _eqLogic.configuration.need_refresh_cron_listener='oui'
+    
     $('.un_arrosage').each(function () {
         var un_arrosage={};
         un_arrosage.id=$(this).attr('id_arrosage');
@@ -506,7 +521,7 @@ function save_arrosage(_eqLogic){
         un_arrosage.liste_end=[];
         un_arrosage.timer='';
         un_arrosage.liste_cd_fin=[];
-
+         //console.log(_eqLogic)
         //declencheur
         $(this).find('.un_declencheur_').each( function() {
             //var un_declencheur=$(this).find('.un_declencheur_item').last().val();
