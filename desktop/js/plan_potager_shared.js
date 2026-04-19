31af39b9da831bee412d2af486a807af8af494af
vegeta0911 <67876957+vegeta0911@users.noreply.github.com>
2026-04-19 18:29:53 +0200

    Update plan_potager_shared.js

--- desktop/js/plan_potager_shared.js
+++ desktop/js/plan_potager_shared.js
@@ -142,9 +142,9 @@ function jardinRenderSemisDetails(plante) {
     if (semis.length === 0) {
         return '<div class="alert alert-warning" style="margin:10px 0 0 0;">Aucun semis archivé pour cette plante.</div>';
     }
-
+    console.log('Semis à afficher :', semis);
     let html = '<table class="table table-condensed" style="margin:10px 0 0 0;">';
-    html += '<tr><th>Nom</th><th>Semis</th><th>QTE Semer</th><th>Germination</th><th>QTE Germer</th><th>Plantation</th><th>QTE Plante</th><th>Récolte</th><th>Poids Récolte</th><th>Commentaire</th></tr>';
+    html += '<tr><th>Nom</th><th>Semis</th><th>QTE</th><th>Germination</th><th>QTE</th><th>Plantation</th><th>QTE</th><th>Eclaircissage</th><th>QTE</th><th>Rempotage</th><th>QTE</th><th>Récolte</th><th>Poids</th><th>Commentaire</th></tr>';
     semis.forEach(function(unSemis) {
         html += '<tr>';
         html += '<td>' + jardinEscapeHtml(unSemis.nom || '') + '</td>';
@@ -154,6 +154,10 @@ function jardinRenderSemisDetails(plante) {
         html += '<td style="text-align:center;">' + jardinEscapeHtml(unSemis.qte_germe || '') + '</td>';
         html += '<td>' + jardinFormatDateFR(unSemis.d_plantation || '') + '</td>';
         html += '<td style="text-align:center;">' + jardinEscapeHtml(unSemis.qte_plante || '') + '</td>';
+        html += '<td>' + jardinFormatDateFR(unSemis.d_eclaircissage || '') + '</td>';
+        html += '<td style="text-align:center;">' + jardinEscapeHtml(unSemis.qte_eclairci || '') + '</td>';
+        html += '<td>' + jardinFormatDateFR(unSemis.d_rempotage || '') + '</td>';
+        html += '<td style="text-align:center;">' + jardinEscapeHtml(unSemis.qte_rempote || '') + '</td>';
         html += '<td>' + jardinFormatDateFR(unSemis.d_recolte || '') + '</td>';
         html += '<td style="text-align:center;">' + jardinEscapeHtml(unSemis.poid_recolte || '') + '</td>';
         html += '<td>' + jardinEscapeHtml(unSemis.commentaire || '') + '</td>';
