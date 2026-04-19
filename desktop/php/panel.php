8a5b2f876c3f61e69785415a574319735a29195a
vegeta0911 <67876957+vegeta0911@users.noreply.github.com>
2026-04-19 18:41:11 +0200

    Update panel.php

--- desktop/php/panel.php
+++ desktop/php/panel.php
@@ -82,6 +82,16 @@ $eqLogics=array_filter($eqLogics, "filtre_potager");
         </div>
       </a>
     </div>
+	<div class="cursor eqLogicAction logoPrimary">
+        <a class="info" href="<?php echo $base_url; ?>/index.php?v=d&m=jardin&p=historique">
+            <i class="fas fa-archive" style="font-size:250%;"></i>
+            <br>
+            <br>
+            <div class="hide_if_mobile">
+                <span>{{Rapport}}</span>
+            </div>
+        </a>
+    </div>
     <div class="cursor eqLogicAction">
 		<a class="info" href=<?php echo $base_url.'/index.php?v=d&m=jardin&p=planning';?>>
 		    <i class="icon kiko-calendar" style="font-size:265%;"></i>
