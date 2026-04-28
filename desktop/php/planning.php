e0a45ef4c31bfffef167116c660244656ef282d1
vegeta0911 <67876957+vegeta0911@users.noreply.github.com>
2026-04-28 12:26:57 +0200

    Update planning.php

--- desktop/php/planning.php
+++ desktop/php/planning.php
@@ -125,6 +125,30 @@ usort($eqLogics, array('jardin','cmp'));
 </div>
           
 <script>
+$('#bt_takeSnapshot').on('click', function() {
+                $.ajax({
+                    type: "POST",
+                    url: "plugins/jardin/core/ajax/jardin.ajax.php",
+                    data: {
+                        action: "takeSnapshot"
+                    },
+                    dataType: 'json',
+                    error: function (request, status, error) {
+                    handleAjaxError(request, status, error);
+                },
+                success: function (data) {
+                if (data.state != 'ok') {
+                    message: 'error' + data.result;
+                    return;
+                }
+                message: 'success' + "{{Le snapshot a été généré et les anciennes données ont été écrasées.}}";
+                setTimeout(function() {
+                location.reload();
+            }, 500);            
+        }
+    });
+});   
+	
 $('#add_item').off('click').on('click', function () {
     setCookie('add_item', 'oui',1);
     window.open(base_url + "/index.php?v=d&m=jardin&p=jardin","_self")
